4fb601416eeafc154b04f726ac6f8d416a021f9d
claudionc <claudionc@ciandt.com>
2019-12-01 11:36:52 -0300

    feet: 05 - 06 - Refatorando o View Model do estado do App

--- app/src/main/java/br/com/alura/aluraesporte/ui/fragment/ListaPagamentosFragment.kt
+++ app/src/main/java/br/com/alura/aluraesporte/ui/fragment/ListaPagamentosFragment.kt
@@ -4,19 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import br.com.alura.aluraesporte.R
 import br.com.alura.aluraesporte.ui.recyclerview.adapter.ListaPagamentosAdapter
+import br.com.alura.aluraesporte.ui.viewmodel.ComponentesVisuais
+import br.com.alura.aluraesporte.ui.viewmodel.EstadoAppViewModel
 import br.com.alura.aluraesporte.ui.viewmodel.PagamentoViewModel
 import kotlinx.android.synthetic.main.lista_pagamentos.*
 import org.koin.android.ext.android.inject
+import org.koin.android.viewmodel.ext.android.sharedViewModel
 import org.koin.android.viewmodel.ext.android.viewModel
 
-class ListaPagamentosFragment : Fragment() {
+class ListaPagamentosFragment : BaseFragment() {
 
     private val adapter: ListaPagamentosAdapter by inject()
     private val viewModel: PagamentoViewModel by viewModel()
+    private val estadoAppviewModel: EstadoAppViewModel by sharedViewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,6 +37,10 @@ class ListaPagamentosFragment : Fragment() {
                 adapter.add(pagamentos)
             }
         })
+        estadoAppviewModel.temComponentes = ComponentesVisuais(
+            appBar = true,
+            bottomNavigation = true
+        )
     }
 
 }
